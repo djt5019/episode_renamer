94bfec18198431344bebbbc574f1e96481e3af75
Dan Tracy <djt5019@psu.edu>
2012-01-25 18:54:08 -0500

    Cleaned up and simplified the logic of the poll_sources module

--- EpParser/src/poll_sources.py
+++ EpParser/src/poll_sources.py
@@ -8,42 +8,49 @@ from os import listdir
 from itertools import ifilter
 
 import Constants
-from Logger import get_logger
+from Logger import get_logger as log
 
 def locate_show(title):
     """Polls the web sources looking for the show"""
     modules = []
 
     sys.path.append(Constants.WEB_SOURCES_PATH)
-    ## This will import all the modules within the web_sources directory so that we
-    ## can easily plug in new sources for finding episode information
-    mods = ifilter( lambda x: x.endswith('py') and not x.startswith('__'),  listdir(Constants.WEB_SOURCES_PATH))
+    # This will import all the modules within the web_sources directory so that 
+    # we can easily plug in new sources for finding episode information
 
-    for m in mods:
-        get_logger().info("Importing web resource {}".format(m[:-3]))
-        x =  __import__(m[:-3])
-        modules.append(x)
+    for m in listdir(Constants.WEB_SOURCES_PATH):
+        if m.endswith('.py') and not m.startswith('__'):
+            log().info("Importing web resource {}".format(m[:-3]))
+            x =  __import__(m[:-3])
+            if not hasattr(x, 'poll'):
+                log().error("Module {} doesn't have a poll method defined... skipping".format(m))
+            modules.append(x)
 
-    #If the modules have a poll priority we will respect it by sorting the list by priority
+    # If the modules have a poll priority we will respect it
+    # by sorting the list by priority, higher number have higher precedence
     if all( hasattr(x, 'priority') for x in modules ):
         modules = sorted(modules, key=lambda x: x.priority, reverse=True)
 
-    get_logger().info("Searching for {}".format(title))
+    log().info("Searching for {}".format(title))
 
     episodes = Constants.SHOW_NOT_FOUND
 
     for source in modules:
-        get_logger().info( "Polling {0}".format(source.__name__) )
+        log().info( "Polling {0}".format(source.__name__) )
 
+        if not hasattr(source, 'poll'):
+            # Only use sources that have the poll method
+            continue
+        
         episodes = source.poll(title)
 
         if episodes:
-            get_logger().info( "LOCATED {0}".format(title) )
+            log().info( "LOCATED {0}".format(title) )
             break
-        get_logger().info( "Unable to locate {0} at {1}".format(title, source.__name__) )
+        log().info( "Unable to locate {0} at {1}".format(title, source.__name__) )
 
     if not episodes:
-        get_logger().info("Unable to locate the show: " + title)
+        log().info("Unable to locate the show: " + title)
         return Constants.SHOW_NOT_FOUND
 
     return sorted(ifilter(lambda x: x.episodeNumber > 0, episodes), key=lambda x:x.episodeCount)
