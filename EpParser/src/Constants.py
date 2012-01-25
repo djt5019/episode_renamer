55ba7183ebbe80b8ee44be804a8201b6b6177237
Dan Tracy <djt5019@psu.edu>
2012-01-25 18:18:26 -0500

    Fixed some issues with the regular expressions, it's much easier to deal with them now

--- EpParser/src/Constants.py
+++ EpParser/src/Constants.py
@@ -9,7 +9,7 @@ from EpParser import __file__ as file_path
 VIDEO_EXTENSIONS = {'.mkv', '.ogm', '.asf', '.asx', '.avi', '.flv', '.mov',
                     '.mp4', '.mpg', '.rm',  '.swf', '.vob', '.wmv', '.mpeg'}
 
-PROJECT_PATH = os.path.abspath(os.path.dirname(os.path.join(file_path)))
+PROJECT_PATH = os.path.abspath(os.path.dirname(file_path))
 RESOURCE_PATH = os.path.join( PROJECT_PATH, 'resources')
 PROJECT_SOURCE_PATH = os.path.join(PROJECT_PATH, 'src')
 WEB_SOURCES_PATH = os.path.join(PROJECT_SOURCE_PATH, 'web_sources')
@@ -21,35 +21,46 @@ SHOW_NOT_FOUND = []
 ## in the _compile_regexs function, otherwise they will not be compiled for simple episode 
 ## information retrieval purposes
 
-_sep = r'[\-\~\.\_\s]'
 _REGEX_VARS = {
-'sep': _sep,
+'sep': r'[\-\~\.\_\s]',
 'sum': r'(.*[\[\(](?P<sum>[a-z0-9]{8})[\]\)])',
 'year': r'(:P<year>(19|20)?\d\d)',
-'episode': r'(e|episode)?{sep}*(?P<episode>\d+)'.format(sep=_sep),
-'season': r'(s|season)?{sep}*(?P<season>\d+)'.format(sep=_sep),
+'episode': r'(e|episode)?{sep}*(?P<episode>\d+)',
+'season': r'(s|season)?{sep}*(?P<season>\d+)',
 'series': r'(?P<series>.*)',
 'subgroup': r'(\[.*\])',
-'special': r'(op|ed|ova|ona|extra|special){sep}*(?P<special>\d+)',
+'special': r'(?P<type>op|ed|ova|ona|extra|special|movie){sep}*(?P<special>\d+)',
 }
+
+for k, v in _REGEX_VARS.iteritems():
+    try:
+        # Substitute any regex variables that may have been used within
+        # later dictionary entries kind of like how let* works in scheme.
+        _REGEX_VARS[k] = v.format(**_REGEX_VARS)
+    except IndexError as e:
+        pass
 
 _UNFORMATED_REGEX = (
             r'^(?P<series>.*?) - Season (?P<season>\d+) - Episode (?P<episode>\d*) - .*',  # Also mine
             r'^(?P<series>.*?) - Episode (?P<episode>\d*) - .*',  # My usual format
-            #r'^{subgroup}*{sep}*?{series}{sep}*{season}{sep}*{episode}{sep}+{sum}?',
-            #r'^{subgroup}*{sep}*?{series}{sep}*{special}{sep}*{sum}?',
-            #r'^{subgroup}*{sep}*?{series}{sep}*{episode}{sep}+{sum}?',
-            #r'^{series}{sep}+{year}?{sep}{season}{sep}*{episode}*{sep}*{sum}?',
-            #r'^{series}{sep}+{year}?{sep}{episode}{sep}*{sum}?',
-            #r'^{series}{sep}+{year}?{sep}{season}X{episode}{sep}*{sum}?',
-            #r'^{series}{sep}+{year}?{sep}\[{season}X{episode}\]{sep}*{sum}?',
-            #r'^(?P<series>.*) - OVA (?P<special>\d+) - \w*',
-            #r'{series}{sep}*{episode}',  # More of a general catch-all regex, last resort
-            #r'{series}{sep}*(op|ed){sep}*(?P<special>\d*){sep}*{sum}?',  # Show intro/outro music, just ignore them
+            r'^{subgroup}*{sep}+{series}{sep}*{season}{sep}*{episode}{sep}+{sum}?',
+            r'^{subgroup}*{sep}+{series}{sep}*{special}{sep}*{sum}?',
+            r'^{subgroup}*{sep}+{series}{sep}*{episode}{sep}+{sum}?',
+            r'^{series}{sep}+{year}?{sep}{season}{sep}*{episode}*{sep}*{sum}?',
+            r'^{series}{sep}+{year}?{sep}{episode}{sep}*{sum}?',
+            r'^{series}{sep}+{year}?{sep}{season}X{episode}{sep}*{sum}?',
+            r'^{series}{sep}+{year}?{sep}\[{season}X{episode}\]{sep}*{sum}?',
+            r'^(?P<series>.*) - OVA (?P<special>\d+) - \w*',
+            r'^{series}{sep}*{special}',
+            r'{series}{sep}+{episode}.*',  # More of a general catch-all regex, last resort
+            r'{series}{sep}*(op|ed){sep}*(?P<special>\d*){sep}*{sum}?',  # Show intro/outro music, just ignore them
             )
 
 ## Substitute the dictionary variables in to the unformated regexes (is the plural of regex, regexes?)
 REGEX =  [r.format(**_REGEX_VARS) for r in _UNFORMATED_REGEX]
+
+del _UNFORMATED_REGEX
+del _REGEX_VARS
 
 NUM_DICT = { '0' : '','1' : 'one', '2' : 'two', '3' : 'three', '4' : 'four', '5' : 'five', '6' : 'six',
         '7' : 'seven', '8' : 'eight', '9' : 'nine', '10' : 'ten', '11' : 'eleven', '12' : 'twelve',
