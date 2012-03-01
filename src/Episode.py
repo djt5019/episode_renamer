a539e55b3777269be04ed918af7fc963e8a3d490
Dan Tracy <djt5019@psu.edu>
2012-03-01 16:33:28 -0500

    fixed a display bug for special episodes

--- src/Episode.py
+++ src/Episode.py
@@ -378,7 +378,7 @@ class EpisodeFormatter(object):
         elif tag in self.episodeCounterTokens:
             if pad:
                 #Total number of digits
-                pad = len(str(self.show.specialsList))
+                pad = len(str(self.show.specialsList[-1].num))
             return str(ep.num).zfill(pad)
 
         elif tag in self.episodeNameTokens:
